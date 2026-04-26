2983d21c44f50228541256c36c3ada6517940d02
Tulasi <tulsishuklag@gmail.com>
2026-04-27 00:16:54 +0530

    backend/config/admindb.ts

---
+++ backend/config/admindb.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import { User } from "../models/userModel";
+
+const MONGO_URI = "mongodb://127.0.0.1:27017/eccomerce"; // 👈 change this
+
+const createAdmin = async () => {
+  try {
+    // ✅ CONNECT DB FIRST
+    await mongoose.connect(MONGO_URI);
+    console.log("✅ DB Connected");
+
+    const existingAdmin = await User.findOne({ email: "admin@gmail.com" });
+
+    if (existingAdmin) {
+      console.log("⚠️ Admin already exists");
+      process.exit();
+    }
+
+    const hashedPassword = await bcrypt.hash("admin123", 10);
+
+    await User.create({
+      name: "Admin",
+      email: "admin@gmail.com",
+      password: hashedPassword,
+      role: "admin",
+      isVerified: true,
+      subscriptionStatus: "active",
+    });
+
+    console.log("✅ Admin created successfully");
+    process.exit();
+
+  } catch (error) {
+    console.log("❌ ERROR:", error);
+    process.exit(1);
+  }
+};
+
+createAdmin();
