fd793d265d37f9f6979cc79911b063525d29ed24
Tulasi <tulsishuklag@gmail.com>
2026-05-21 18:34:25 +0530

    add utils/sendEmail.ts

--- backend/utils/sendEmail.ts
+++ backend/utils/sendEmail.ts
@@ -1,20 +1,54 @@
 
+// import nodemailer from "nodemailer";
+
+// export const sendEmail = async (to: string, subject: string, text: string) => {
+
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from: process.env.EMAIL_USER,
+//     to,
+//     subject,
+//     text,
+//   });
+// };
+
+
+
+
 import nodemailer from "nodemailer";
 
-export const sendEmail = async (to: string, subject: string, text: string) => {
+export const sendEmail = async (
+  to: string,
+  subject: string,
+  text: string
+) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text,
+    });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text,
-  });
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("MAIL ERROR:", error);
+  }
 };
