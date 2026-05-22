73b736854ec0bb6b3f33638e3e10c79bd6e02732
Tulasi <tulsishuklag@gmail.com>
2026-05-22 23:37:06 +0530

    utils/sendEmail.ts

--- backend/utils/sendEmail.ts
+++ backend/utils/sendEmail.ts
@@ -1,5 +1,40 @@
 
 
+// import nodemailer from "nodemailer";
+
+// export const sendEmail = async (
+//   to: string,
+//   subject: string,
+//   text: string
+// ) => {
+//   try {
+//     console.log("EMAIL_USER:", process.env.EMAIL_USER);
+//     console.log("Sending email to:", to);
+
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     const info = await transporter.sendMail({
+//       from: process.env.EMAIL_USER,
+//       to,
+//       subject,
+//       text,
+//     });
+
+//     console.log("Email sent:", info.response);
+
+//   } catch (error: any) {
+//     console.log("EMAIL ERROR:", error.message);
+//     console.log(error);
+//   }
+// };
+
+
 import nodemailer from "nodemailer";
 
 export const sendEmail = async (
@@ -8,16 +43,24 @@ export const sendEmail = async (
   text: string
 ) => {
   try {
-    console.log("EMAIL_USER:", process.env.EMAIL_USER);
-    console.log("Sending email to:", to);
-
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+
+      tls: {
+        rejectUnauthorized: false,
+      },
     });
+
+    await transporter.verify();
+
+    console.log("SMTP VERIFIED");
 
     const info = await transporter.sendMail({
       from: process.env.EMAIL_USER,
@@ -26,11 +69,9 @@ export const sendEmail = async (
       text,
     });
 
-    console.log("Email sent:", info.response);
+    console.log("EMAIL SENT:", info.response);
 
-  } catch (error: any) {
-    console.log("EMAIL ERROR:", error.message);
-    console.log(error);
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
   }
 };
-
